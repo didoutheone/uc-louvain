9839a33e2c0970cf882e41e0459c9207b11982a3
Clementine <clementine.thornary@gmail.com>
2020-05-01 15:06:59 +0200

    testing generator and displayer

---
+++ tests/courseGeneratorTest.py
@@ -0,0 +1,24 @@
+import unittest
+
+from main.courseGen.exposition.displayCourse import DisplayCourse
+from main.courseGen.exposition.generatorRequest import GeneratorRequest
+from main.courseGen.model.description import Description
+from main.courseGen.model.volume import Volume
+from main.courseGen.use_case.generateCourse import GenerateCourse
+
+
+class GenerateCourseTest(unittest.TestCase):
+
+    def test_generate_course(self):
+        profilId = 1
+        descriptions = Description("du droit", "test2", "gros exam")
+        volumes = Volume(20, 5)
+        request = GeneratorRequest(profilId, descriptions, volumes)
+        generator = GenerateCourse(request)
+        generator.generate()
+        self.assertEqual(generator.course.status, "created")
+
+    def test_display_course(self):
+        courseId = 'LDROI1623'
+        data = "Id cours : LDROI1623\nId professeur: 1\nDescription du cours :\n\tContenu : du droit\n\tAcquis : plus de droit\n\tMethode d'évaluation : gros exam\nVolumes horaires :\n\tCours magistraux : 20 heures\n\tTravaux pratiques : 5 heures\n"
+        self.assertEqual(DisplayCourse(courseId).displayCourse(), data)
