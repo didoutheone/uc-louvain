770699e4da9f0febb1eb5aee9c892df239cdc93e
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-05-03 23:30:56 +0200

    cleanup

--- main/common/dto/teacherData.py
+++ main/common/dto/teacherData.py
@@ -5,7 +5,6 @@ class TeacherData:
     def __init__(self, id, faculty):
         self.id = id
         self.faculty = faculty
-        self.file = "/home/clementine/Documents/Soat/formation_ddd/uc-louvain/db/teachers"
 
     def getFaculty(self):
         return self.faculty
